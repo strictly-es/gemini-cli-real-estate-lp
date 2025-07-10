2abb6622de0c326f1058ed6ab9609bdbb77b91c1
大村俊輔 <oomurashunsuke@oomurashunsukenoMacBook-Air.local>
2025-07-10 17:44:57 +0900

    Integrate Formspree for contact form

--- components/Contact.js
+++ components/Contact.js
@@ -4,20 +4,20 @@ const Contact = () => {
   return (
     <Container id="contact" className="py-5 bg-light">
       <h2 className="text-center mb-4">Contact Us</h2>
-      <Form>
+      <Form action="https://formspree.io/f/mblynjnq" method="POST">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" name="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name" />
+          <Form.Control type="text" name="name" placeholder="Name" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicMessage">
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" name="message" rows={3} />
         </Form.Group>
 
         <Button variant="primary" type="submit">
